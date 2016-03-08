86018319dfd3cb75311dd10cbd3585927dd83beb
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-03-08 17:43:26 +0530

    Packed all functions to a class

--- main_crawler.py
+++ main_crawler.py
@@ -3,26 +3,33 @@ import urllib.request
 from queue import Queue
 from bs4 import BeautifulSoup
 
-q = Queue()
 
-def crawl(url):
-    response = urllib.request.urlopen(url)
-    html_bytes = response.read()
-    soup = BeautifulSoup(html_bytes,'html.parser')
-    for link in soup.findAll('a'):
-        l = link.get('href')
-        if l != None:
-            if "http://www.iitg.ernet" in l and ".pdf" not in l:
-                q.put(l)
-                print(l+"is addd to queue")
-    while not q.empty():
-        link_to_crawl = q.get()
-        print(link_to_crawl)
-        crawl(link_to_crawl)
+class MainCrawler:
 
+    def __int__(self, search_key_word):
+        self.to_search = search_key_word
+        self.list_of_links_to_crawl = Queue()
+        self.crawled_links = {"#"}
 
+    def crawl(self, url):
+        response = urllib.request.urlopen(url)
+        html_bytes = response.read()
+        soup = BeautifulSoup(html_bytes,'html.parser')
 
+        for link in soup.findAll('a'):
+            l = link.get('href')
+            if l is not None:
+                if l not in self.crawled_links:
+                    if "http://www.iitg.ernet" in l and ".pdf" not in l:
+                        self.list_of_links_to_crawl.put(l)
+                        print(l+"is addd to queue")
+        while not self.list_of_links_to_crawl.empty():
+            link_to_crawl = self.list_of_links_to_crawl.get()
+            print(link_to_crawl)
+            self.crawled_links.add(link_to_crawl)
+            self.crawl(link_to_crawl)
 
-crawl("http://iitg.ernet.in")
-#crawl("http://www.iitg.ernet.in/director/dirmsg.html")
+    def tryit(self):
+        self.crawl("http://iitg.ernet.in")
+
 
