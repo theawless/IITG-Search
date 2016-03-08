67a2cb410c4a622c71a8c939455399415db955c3
abhinav.prince2012 <abhinav.prince2012@gmail.com>
2016-03-08 06:39:45 +0530

    Added setlog

---
+++ setlog.py
@@ -0,0 +1,30 @@
+
+
+#!/usr/bin/python3
+
+import logging
+logger = logging.getLogger()
+
+def setupLogger():
+
+    #setting format of log
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    logger.setLevel(logging.DEBUG)
+    #file location
+    debug_log='/poop.txt'
+    
+    #adding handler for console logs
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    
+    #adding handler for file logs
+    fh = logging.FileHandler(debug_log)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    
+    logger.debug('SETLOG logger setup done')
+   
+setupLogger()
+
+
